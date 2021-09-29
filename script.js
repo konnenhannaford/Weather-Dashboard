aaf71d667f7fdfef1ab3dd6213937c9c3dc3e5b6
konnenhannaford <konnen23@gmail.com>
2021-09-30 07:52:46 +1000

    updated design to show 5 day forecast calendar

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// this is from nicks tutorial examplre-------
+// this is from nicks tutorial example-------
 // var locationEl = document.querySelector("#location");
 // var tempVal = document.querySelector("#temp");
 // var humidVal = document.querySelector("#humid");
@@ -49,12 +49,11 @@ $(document).ready(onReady)
 
 function onReady() {
    $('#submitButton').on('click',onClickSubmitButton)
-   renderCityButttons()
+   renderCityButtons()
 }
 
-// $(document).ready(function(){
-//     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-//     createTimeRows()
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    // createTimeRows()
 
 var storedSearches = JSON.parse(localStorage.getItem("storageArray"));
 let mySearchedList = storedSearches || []
@@ -63,38 +62,99 @@ function onClickSubmitButton() {
     let myText = $('#inputEl').val()
     mySearchedList.push(myText)
     localStorage.setItem("storageArray", JSON.stringify(mySearchedList))
-    renderCityButttons()
+    renderCityButtons()
     getWeather(myText)
 }
 
-function renderCityButttons() {
+
+// this will search through the array to locate the student.
+$('#submitButton').on('click', function(){
+    //stores users search into a variable
+    myText = $('#inputEl').val();
+
+    mySearchedList = []; // <<<<<<<<<<<<<<< HERE
+
+    //search through array here...
+    for (var i = 0; i < mySearchedList.length; i++) {
+        storedSearches = mySearchedList[i].myText;
+    }
+
+function onClickSubmitButton2(val) {
+    var value  = val;   
+    mySearchedList.push(value)
+    localStorage.setItem("storageArray", JSON.stringify(mySearchedList))
+    renderCityButtons()
+    getWeather(value)
+}
+
+
+// function renderCityButtons() {
+//     let myHistory = $("#searchHistory")
+//     myHistory.empty()
+//     mySearchedList.forEach(element => {
+//         let buttonEL = $("<button/>").appendTo(myHistory)
+//         $(buttonEL).on('click',onClickHistoryButtons)
+//         $(buttonEL).addClass("btn btn-outline-primary col-12")
+//         $(buttonEL).text(element)
+//     });
+// }
+
+function renderCityButtons() {
     let myHistory = $("#searchHistory")
     myHistory.empty()
+    var button = "";
     mySearchedList.forEach(element => {
-        let buttonEL = $("<button/>").appendTo(myHistory)
-        $(buttonEL).on('click',onClickHistoryButtons)
-        $(buttonEL).addClass("btn btn-outline-primary col-10")
-        $(buttonEL).text(element)
+       //  let buttonEL = $("<button>").appendTo(myHistory)
+       // // $(buttonEL).on('click',onClickHistoryButtons)
+
+       // console.log(buttonEL);
+
+       //  $(buttonEL).addClass("btn btn-outline-primary col-12 historybtn")
+       //  $(buttonEL).text(element)
+    button +='<button class="btn btn-outline-primary col-12 historybtn" onclick="onClickSubmitButton2('+"'"+element+"'"+')">'+element+'</button>'
+
+      // console.log(element)
+
     });
+
+    myHistory.html(button);
 }
+
+$("#searchHistory").on("click","historybtn",function(){
+    alert("")
+    var city  = $(this).html();
+    alert(city);
+
+})
 
 function onClickHistoryButtons() {
     console.log(this)
 }
 
 function getWeather(cityName) {
-    let myUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=f91eaf46e0bf96dd19136475c9928ef1"
+    let myUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=f91eaf46e0bf96dd19136475c9928ef1";
+    
+
     $.ajax({
         type:"GET",
         url:myUrl,
     }).then(response=>{
+
+
+        
+
+
         let date = response.list[0].dt_txt
+
+        console.log(date)
+
         let icon = response.list[0].weather[0].icon
         let iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
         let mycard = 
-        $(`<div class="card-body">
+        $(`<div class="card"><div class="card-body">
         <h5 class="card-title">${date}</h5>
         <div><img src="${iconUrl}" alt="weather icon"></div>
-        </div>`)
+        </div></div>`)
         $("#mycard").append(mycard)
-    })}
+    })}})
+
