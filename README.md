8e2a703d746cb245ee38bcbec189ba60d02f1560
konnenhannaford <konnen23@gmail.com>
2021-10-02 01:41:06 +1000

    finalised project to show future dates

--- README.md
+++ README.md
@@ -1,31 +1,4 @@
 # Weather-Dashboard
-
-When users earch a city, they are presented with info on current weather coniditions
-When users earch a city, there searches ar elisted on the age and can be clicked on again to re-show current conditions
-When users searchg a city, they are presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-```
-
-
-current and future conditions 
-
-
-the date, 
-and the UV index -  with a color that indicates whether the conditions are favorable, moderate, or severe
-
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the 
-date, 
-an icon representation of weather conditions, 
-the temperature, 
-the wind speed, 
-and the humidity
-
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
-
-# Weather Dashboard
-
 Site can be accessed here: https://github.com/konnenhannaford/Weather-Dashboard
 Github Repo: The source can be retrieved by cloning and pulling from https://konnenhannaford.github.io/Weather-Dashboard/
 
@@ -37,16 +10,16 @@ This page is a weather page that allows users to show teh weather froma round th
 
 # Updates
 
-- When users earch a city, they are presented with info on current weather coniditions
-- When users earch a city, there searches ar elisted on the age and can be clicked on again to re-show current conditions
-- When users searchg a city, they are presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+- When users search a city, they are presented with info on current weather coniditions
+- When users search a city, there searches are listed on the page and can be clicked on again to re-show current conditions
+- When users search a city, they are presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index for today
+- When users searchg a city, they are presented with the an icon representation of weather conditions, the temperature, the humidity, for the next 5 days
 
 # Making Changes
 - Repo is public and can be updated by all via https://github.com/konnenhannaford/Weather-Dashboard
 
 # Learnings and changes
 Completing this has increased familiarity with Bootstrap.  I really struggle with Javascript though and have taken time off to ensure that this doesnt happen again.  updates that still need to be made incldue:
-- A working 5 day forecast
 - UV Index being able to be updated on colour
 - Ability to replace current search with new search
 - Update to include future work
--- script.js
+++ script.js
@@ -69,11 +69,14 @@ function getWeather(cityName) {
         type:"GET",
         url:firstCall,
     }).then(response1=>{
+        
+
     let secondCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${response1.coord.lat}&lon=${response1.coord.lon}&appid=f91eaf46e0bf96dd19136475c9928ef1&units=metric`;
     $.ajax({
         type:"GET",
         url:secondCall,
     }).then(response2=>{
+        
 
         let date = new Date().toDateString()
         let cityname = response1.name;
@@ -94,7 +97,85 @@ function getWeather(cityName) {
         <div><p>Temp: ${temp} °C</p></div>     
         <div><p>Humidity: ${humidity} %</p></div>
         <div><p>Wind Speed: ${wind} km/h</p></div>
-        <div><p id="uviIndex">UVI Index: ${uvi} INDEX</p></div>`
+        <div><p id="uviIndex">UVI Index: ${uvi} </p></div>`
         )
         $("#mycard").append(mycard)
+
+
+        var mycard2 = ""
+        
+        for (let i=1; i < 6; i++){
+          
+          
+
+            // cityname = response1.name;
+           
+            var humidity3 = response2.daily[i]['humidity'];
+            var temp3 = response2.daily[i].temp['day'];
+            var wind3 = response2.daily[i].wind;
+            var iconUrl3 = response2.daily[i].weather[0]['icon'];
+            //var date3 = response2.daily[i]['date'];
+
+              iconUrl3 = "https://openweathermap.org/img/wn/" + iconUrl3 + "@2x.png"
+            //  temp = response2.current.temp;     
+            //  humidity = response2.current.humidity;
+            //  wind = response2.current.wind_speed;
+            //  uvi = response2.current.uvi;
+
+         
+
+
+
+
+        mycard2 += 
+        
+        `<div class="col-2" >
+        <div class="card">
+        <div class="card-body">
+        <h1 class="card-title red"></h1>
+        <div><img src="${iconUrl3 }"alt="weather icon"></div>
+        <div><p>Temp: ${temp3} °C</p></div>     
+        <div><p>Humidity: ${humidity3 } %</p></div>
+        <div><p>Wind Speed: ${wind3} km/h</p></div></div></div></div>`;
+    
+          } 
+
+          $("#mycard2").append(mycard2)
+
         })})}
+
+// . then(response=>{
+ 
+    
+    // 5 day forecast
+    
+    
+//   })
+//   })
+//   }        
+          
+          // to make uv index
+{/* <div>
+<span id="uvi" class="uv-index py-1 px-2 rounded-md"></span>
+</div> 
+var currentClass;
+
+function uvcolours () {
+if (currentClass) uviVal.classList.remove(currentClass);
+
+if (uvi <= 2) {
+uviVal.classList.add("bg-green-300");
+currentClass = "bg-green-300";
+} else if (uvi <= 5) {
+uviVal.classList.add("bg-yellow-300");
+currentClass = "bg-yellow-300";
+} else if (uvi <= 7) {
+uviVal.classList.add("bg-yellow-600");
+currentClass = "bg-yellow-600";
+} else if (uvi <= 10) {
+uviVal.classList.add("bg-red-500");
+currentClass = "bg-red-500";
+} else {
+uviVal.classList.add("bg-red-700");
+currentClass = "bg-red-700";
+}} */}
